ffb5b7708860e2cc4f772585981ee66535d441c8
kmyali <kmali@mun.ca>
2022-07-26 23:09:28 -0230

    Working version to a good extent

--- conc_assign4.py
+++ conc_assign4.py
@@ -1,8 +1,9 @@
-import re
+import sys
 from mpi4py import MPI
 import numpy as np
 from copy import deepcopy
 import time
+import trace 
 
 PIXELS_PER_ROW = 64*64*4
 comm = MPI.COMM_WORLD
@@ -20,7 +21,6 @@ KERNEL = [
 
 def main():
     
-    # TODO uncomment
     # Read the image
     if comm.rank == 0:
         with open("pepper.ascii.pgm", "r") as f:
@@ -34,105 +34,210 @@ def main():
     image = comm.bcast(image,0) 
 
     # Each node has a subimage
-    subimage = [ [0]*64 for col in range(64) ]
-
-    # NUMPY
-    ########################################################
-    # subimages = np.ndarray(shape=(4,4))
-    # for i,subimage in enumerate(subimages):
-    #     subimage = np.ndarray(shape=(64,64), dtype=int)
-    #     print(i,subimage.shape)
-    ########################################################
-
-    # Add padding?
+    subimage = np.zeros((72,72), dtype = int)
+    final_subimage = np.zeros((64,64), dtype = int)
 
     rank = comm.rank
-    # DEBUG rank = 0
-
-    # Populate subimage
+    # Populate subimage with core pixels
     for current_row in range(64):
         for current_col in range(64):
             current_pixel = find_pixel_id(rank,current_row, current_col)
-            subimage_row = subimage[current_row]
-            subimage_row[current_col] = image[current_pixel]
+            subimage[current_row+4, current_col+4] = image[current_pixel]
 
-    # print("rank ", rank, "subimage", subimage)
+    # Populate subimage with non-core pixels on 2 steps
+    # STEP 1: SEND/RECEIVE top/bottom/left/right
+    # even row/even col and odd row/odd col 
+    if ( (rank // 4) % 2 + (rank % 4) % 2 ) % 2 == 0:
+
+        # Receive from the right (rank + 1) a 64x4 block then send, unless im the right column
+        if (rank % 4 != 3) and within_image(rank+1):
+            cols = comm.recv(source = rank + 1)
+            subimage[4:68, 68:72] = cols
+
+            cols = subimage[4:68, 64:68]
+            comm.send(cols, dest = rank + 1)
+
+        # Receive from the left (rank - 1) a 64x4 block then send, unless left column
+        if (rank % 4 != 0) and within_image(rank-1):
+            cols = comm.recv(source = rank - 1)
+            subimage[4:68, 0:4] = cols
+
+            cols = subimage[4:68, 4:8]
+            comm.send(cols, dest = rank - 1)
+        
+        # Receive from the top (rank - 4) a 4x64 block then send, unless top row, included in within_image
+        if within_image(rank-4):
+            rows = comm.recv(source = rank - 4)
+            subimage[0:4, 4:68] = rows
+
+            rows = subimage[4:8, 4:68]
+            comm.send(rows, dest = rank - 4)
+        
+        # Receive from the bottom (rank + 4) a 4x64 block then send, unless botttom row
+        if within_image(rank+4):
+            rows = comm.recv(source = rank + 4)
+            subimage[68:72, 4:68] = rows
+
+
+            rows = subimage[64:68, 4:68]
+            comm.send(rows, dest = rank + 4)
     
-    # Apply filter and request data where needed
-    final_subimage = [ [0]*64 for col in range(64) ]
+    # even row/odd col and odd row/even col
+    else:
+        # Send to left (rank - 1) a 64x4 block then receive, unless im left column
+        if (rank % 4 != 0) and within_image(rank-1):
+            cols = subimage[4:68, 4:8]
+            comm.send(cols, dest = rank - 1)
 
-    for current_row in range(64):
-        for current_col in range(64):
+            cols = comm.recv(source = rank - 1)
+            subimage[4:68, 0:4] = cols
+        
+        # Send to (rank + 1) a 64x4 block then receive
+        if (rank % 4 != 3) and within_image(rank+1):
+            cols = subimage[4:68, 64:68]
+            comm.send(cols, dest = rank + 1)
+
+            cols = comm.recv(source = rank + 1)
+            subimage[4:68, 68:72] = cols
+        
+        # Send to (rank + 4) a 4x64 block then receive
+        if within_image(rank+4):
+            rows = subimage[64:68, 4:68]
+            comm.send(rows, dest = rank + 4)
+
+            rows = comm.recv(source = rank + 4)
+            subimage[68:72, 4:68] = rows
+
+        # Send to (rank - 4) a 4x64 block  then receive
+        if within_image(rank - 4):
+            rows = subimage[4:8, 4:68]
+            comm.send(rows, dest = rank - 4)
+
+            rows = comm.recv(source = rank - 4)
+            subimage[0:4, 4:68] = rows
+
+    # STEP 2: SEND/RECEIVE diagonals
+    # Start with even rows
+    if (rank // 4) % 2 == 0:
+        
+        # Bottom right
+        if (rank % 4 != 3) and (rank // 4 != 3):
+            subarr = comm.recv(source = rank + 5)
+            subimage[68:72, 68:72] = subarr
+
+            subarr = subimage[64:68, 64:68]
+            comm.send(subarr, dest = rank + 5)
+
+        # Bottom left
+        if (rank % 4 != 0) and (rank // 4 != 3):
+            subarr = comm.recv(source = rank + 3)
+            subimage[68:72, 0:4] = subarr
+
+            subarr = subimage[64:68, 4:8]
+            comm.send(subarr, dest = rank + 3)
+
+        # Top right
+        if (rank % 4 != 3) and (rank // 4 != 0):
+            subarr = comm.recv(source = rank - 3)
+            subimage[0:4, 68:72] = subarr
+
+            subarr = subimage[4:8, 64:68]
+            comm.send(subarr, dest = rank - 3)
+
+        # Top left
+        if (rank % 4 != 0) and (rank // 4 != 0):
+            subarr = comm.recv(source = rank - 5)
+            subimage[0:4, 0:4] = subarr
+
+            subarr = subimage[4:8, 4:8]
+            comm.send(subarr, dest = rank - 5)
+    
+    # Odd rows
+    else:
+        # Top left
+        if (rank % 4 != 0) and (rank // 4 != 0):
+            subarr = subimage[4:8, 4:8]
+            comm.send(subarr, dest = rank - 5)
+
+            subarr = comm.recv(source = rank - 5)
+            subimage[0:4, 0:4] = subarr
+
+        # Top right
+        if (rank % 4 != 3) and (rank // 4 != 0):
+            subarr = subimage[4:8, 64:68]
+            comm.send(subarr, dest = rank - 3)
+
+            subarr = comm.recv(source = rank - 3)
+            subimage[0:4, 68:72] = subarr
+        
+        # Bottom left
+        if (rank % 4 != 0) and (rank // 4 != 3):
+            subarr = subimage[64:68, 4:8]
+            comm.send(subarr, dest = rank + 3)
             
-            # print("rank", rank, "row", current_row, "col", current_col)
-            
+            subarr = comm.recv(source = rank + 3)
+            subimage[68:72, 0:4] = subarr      
+        
+        # Bottom right
+        if (rank % 4 != 3) and (rank // 4 != 3):
+            subarr = subimage[64:68, 64:68]
+            comm.send(subarr, dest = rank + 5)
+
+            subarr = comm.recv(source = rank + 5)
+            subimage[68:72, 68:72] = subarr
+
+    # # For debugging
+    # for i in range(16):
+    #     if i != rank:
+    #         time.sleep(2)
+    #     else:
+    #         print("rank", rank, "subimage", subimage)        
+
+# At this point all nodes should have all the values they need, compute the filter
+    for current_row in range(4,68):
+        for current_col in range(4,68):  
+        
+            # For each pixel, perform 81 multiplications
             for i in range(-4, 5):
                 for j in range(-4,5):
+                    final_subimage[current_row-4, current_col-4] += subimage[current_row + i, current_col + j] * KERNEL[i+4][j+4]
+            
+            # After applying kernel on a pixel, clip the value if beyond limits
+            current_value = final_subimage[current_row-4, current_col-4]
+            if current_value < 0:
+                final_subimage[current_row-4, current_col-4] = 0
+            elif current_value > 255:
+                final_subimage[current_row-4, current_col-4] = 255
 
-                    # before i receive and block, probe to see if someone wants a message from me
-                    status = MPI.Status()
-                    probe = comm.iprobe(status = status)
-                    if probe:
-                        buf = np.zeros(1, dtype=int)
-                        r = comm.Irecv(buf, source = status.Get_source(), tag = status.Get_tag()) # since we know there is a message, no waiting request
-                        r.Wait(status)
-                        print(rank, "recieved request with message", buf[0], "for pixel id", status.Get_tag(), "from", status.Get_source())
-                        
-                        # TODO fix: Asssuming this is always another node requesting a pixel 
-                        [requested_pixel_rank, requested_pixel_row, requested_pixel_col] = find_subimage(buf[0])
-                        p_value = subimage[requested_pixel_row][requested_pixel_col]
-                        p = np.array([p_value], dtype=int)
-                        comm.Isend(p, dest = status.Get_source(), tag = status.Get_tag())
-                        print(rank, "sent message for pixel id", status.Get_tag(), "to", status.Get_source())
+    final_image = np.zeros((256,256), dtype = int)
+    # Gather the image in one array
+    if rank != 0:
+        comm.send(final_subimage, dest=0)
 
-                    
-                    if (current_row+i in range(64) and current_col+j in range(64)):
-                        final_subimage[current_row][current_col] += subimage[current_row + i][current_col + j] * KERNEL[4 + i][4 + j]
-                        # print("value without requests", final_subimage[current_row][current_col])
+    else:
+        final_image[0:64, 0:64] = final_subimage
 
-                    else:
-                        requested_pixel_id = find_pixel_id(rank, current_row + i, current_col + j)
-                        # print("req id", requested_pixel_id)
-                        if requested_pixel_id == -1:
-                            continue
-                        [requested_pixel_rank, requested_pixel_row, requested_pixel_col] = find_subimage(requested_pixel_id)
-                        # print("rank", rank, " found pixel", requested_pixel_id, "in rank", requested_pixel_rank)
-                    
-                        # This send should be sync?
-                        r_id = np.array([requested_pixel_id], dtype=int)
-                        comm.Isend(r_id, dest=requested_pixel_rank, tag = requested_pixel_id) 
-                        # print(rank, "requested pixel ", requested_pixel_id, "from", requested_pixel_rank)
+        for i in range(1,16):
+            subimage = comm.recv(source=i)
+            
+            row_begin = (i // 4) * 64
+            row_end = row_begin + 64
+            col_begin = (i % 4) * 64
+            col_end = col_begin + 64
 
-                        pixel_value = np.zeros(1, dtype=int)
-                        req = comm.Irecv(pixel_value, source = requested_pixel_rank, tag = requested_pixel_id)
-                        req.Wait()
-                        print(rank, "reieved pixel id", requested_pixel_id, "from", requested_pixel_rank)
-                        
-                        final_subimage[current_row][current_col] += pixel_value[0] * KERNEL[4+i][4+j]
+            print("rank", i, "values:", row_begin, row_end, col_begin, col_end)
 
-            if final_subimage[current_row][current_col] < 0:
-                final_subimage[current_row][current_col] == 0
-            elif final_subimage[current_row][current_col] > 255:
-                final_subimage[current_row][current_col] == 255
-        # TODO: how to ensure nodes stay alive? need them alive because others may request. BARRIER? 
+            final_image[row_begin:row_end, col_begin:col_end] = subimage
     
-    # Writing the output
-    print("rank", rank, "started writing o/p")
-    with open("output.ascii.pgm", "w") as f:
-        
-        f.write("P2")
-        f.write("256 256")
-        f.write("255")
-        
-        f.write(" ".join(final_image))
-                    
-
-    # Translate the pixel coordinates (current_row + i, current_col + j) to global coordinates
-    # figure out which subimage it belongs to
-    # create a request (we do need a send and a receive)
-    ## OR can pre-calculate all the columns and rows each rank needs, but how to save them? can add three columns and rows of padding on each side?
-
-    # if subimage(something) > max or < min, clip
+            # Write the output
+            with open("output.ascii.pgm", "w") as f:
+                
+                f.write("P2\n")
+                f.write("256 256\n")
+                f.write("255\n")
+                with np.printoptions(threshold=np.inf):
+                    x = str(list(final_image.reshape(-1)))
+                    f.write( x.replace(",", "").replace("[", "").replace("]", ""))
 
 # subimage_index would be the requester's rank. should pass (current_row + i) as current_row
 def find_pixel_id(subimage_index, current_row, current_col):
@@ -148,6 +253,8 @@ def find_pixel_id(subimage_index, current_row, current_col):
     pixels_curr_row_same_subimage = current_col
 
     global_id = pixels_prev_rows + pixels_curr_row_prev_subimages + pixels_curr_row_same_subimage
+    if global_id > 65535:
+        return -1
     return global_id
 
 #subimage_id would be the destination rank
@@ -159,37 +266,7 @@ def find_subimage(global_id):
     col_within_sub_image = global_id % 64
     return [subimage_id, row_within_subimage, col_within_sub_image]
 
+def within_image(rank):
+    return rank <= 15 and rank >=0
+
 main()
-
-
-
-# Each node defines a 72 x 72 array with the 64 x 64 subimage in the middle
-# Each node wants to fill out the empty spots
-# Consider node 5
-# For the top 4 rows we know that it is going to be receiving
-# For the next 4 rows, except for first and last 4 pixels, we know that is it going to be sending (potentially to multiple nodes: 0,1,2,4,6,8,9,10)
-# can have the node 5 receiving from ANY_SOURCE node because there are other nodes sending to node 5 but node 5 doesnt know when they will send
-# use isend and irecv with the tag number equal global pixel id wanted
-
-# Node 1 will have
-# comm.Isend([requested_pixel_row, requested_pixel_col], dest=requested_pixel_rank, tag = requested_pixel_id)
-
-# Node 5 will have
-# while (???):
-#   for requested_pixel_id in pixel_ids:
-#       comm.Irecv([requested_pixel_row, requested_pixel_col], source=ANY_SOURCE, tag = requested_pixel_id)
-
-# for loop over each
-
-# for current_rank in range(16):
-#     if comm.rank == current_rank:
-
-#         find_pixel_you_want
-#         comm.recv(pixel)
-
-#     else:
-#         while(??):
-#             if irecv is not null
-#                 comm.send(the_requested_pixels) 
-
-#After this for loop, every subimage would have all the pixels it needs to calculate LoG
